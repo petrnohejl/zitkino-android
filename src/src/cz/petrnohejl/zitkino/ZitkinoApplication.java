1e16ea8610d96fb2c97c1755c73a598dfa9c4ee8
Petr Nohejl <petr.nohejl@gmail.com>
2012-10-25 18:36:11 +0200

    Application class.

--- src/src/cz/petrnohejl/zitkino/ZitkinoApplication.java
+++ src/src/cz/petrnohejl/zitkino/ZitkinoApplication.java
@@ -5,15 +5,35 @@ import android.content.Context;
 
 public class ZitkinoApplication extends Application
 {
-	private static ZitkinoApplication instance;
+	private static ZitkinoApplication mInstance;
+
 
 	public ZitkinoApplication()
 	{
-		instance = this;
+		mInstance = this;
 	}
+
+
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+		
+		// force AsyncTask to be initialized in the main thread due to the bug:
+		// http://stackoverflow.com/questions/4280330/onpostexecute-not-being-called-in-asynctask-handler-runtime-exception
+		try
+		{
+			Class.forName("android.os.AsyncTask");
+		}
+		catch(ClassNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
 
 	public static Context getContext()
 	{
-		return instance;
+		return mInstance;
 	}
 }
